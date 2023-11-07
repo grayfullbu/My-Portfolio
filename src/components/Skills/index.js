167005ca103b54ff0d34e72d970a640f7c516fa8
akodiayni <118144040+akodiayni@users.noreply.github.com>
2023-11-07 08:29:39 +0800

    chage provide

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -86,7 +86,7 @@ export default function Skills() {
     <>
     <Aboutbottom>
     <div className='provide'>
-    <div className='what'> What I Can Providessdasdsa</div>
+    <div className='what'> What I Can Providefgfdgfdg</div>
     <button onClick={toggleModal} className="btn-modal">
       <div  className='Front_end'>
         <div className='iconss'>
